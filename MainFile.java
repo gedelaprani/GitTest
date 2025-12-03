60489130fe89da87cb7e37a7604e3e345bf13fe7
pgedela12 <gedelaprani@gmail.com>
2025-12-03 11:43:46 -0500

    Single Thread Capitalize

--- MainFile.java
+++ MainFile.java
@@ -1,1 +1,43 @@
 //Ishaan and Praneeth 12/3
+import java.io.*;
+
+public class MainFile {
+    public static void main(String[] args) {
+        File inputFile = new File("pg2701.txt");
+        File outputFile = new File("capitalized_output1.txt"); // created behind the scenes
+
+        try (
+            BufferedReader reader = new BufferedReader(new FileReader(inputFile));
+            BufferedWriter writer = new BufferedWriter(new FileWriter(outputFile))
+        ) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                writer.write(line.toUpperCase());  // capitalize all letters
+                writer.newLine();
+            }
+
+            System.out.println("Created file: " + outputFile.getAbsolutePath());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        File inputFile2 = new File("pg10.txt");
+        File outputFile2 = new File("capitalized_output2.txt"); // created behind the scenes
+
+        try (
+            BufferedReader reader = new BufferedReader(new FileReader(inputFile2));
+            BufferedWriter writer = new BufferedWriter(new FileWriter(outputFile2))
+        ) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                writer.write(line.toUpperCase());  // capitalize all letters
+                writer.newLine();
+            }
+
+            System.out.println("Created file: " + outputFile2.getAbsolutePath());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}

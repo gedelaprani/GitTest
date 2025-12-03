8ddd4ff0ce002afd94f0f99ab9534d3961e8c532
ishaannair17-png <ishaan.nair17@gmail.com>
2025-12-03 12:01:34 -0500

    Used Two Threads

--- MainFile.java
+++ MainFile.java
@@ -1,43 +1,62 @@
-//Ishaan and Praneeth 12/3
+// Ishaan and Praneeth 12/3
 import java.io.*;
 
 public class MainFile {
+
+    static class CapitalizeFileTask implements Runnable {
+        private String inputFileName;
+        private String outputFileName;
+
+        public CapitalizeFileTask(String inputFileName, String outputFileName) {
+            this.inputFileName = inputFileName;
+            this.outputFileName = outputFileName;
+        }
+
+        public void run() {
+            File inputFile = new File(inputFileName);
+            File outputFile = new File(outputFileName);
+
+            try (BufferedReader reader = new BufferedReader(new FileReader(inputFile));
+                 BufferedWriter writer = new BufferedWriter(new FileWriter(outputFile))) {
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    writer.write(line.toUpperCase());
+                    writer.newLine();
+                }
+
+                System.out.println("Created file: " + outputFile.getAbsolutePath());
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
     public static void main(String[] args) {
-        File inputFile = new File("Bible.txt");
-        File outputFile = new File("capitalized_output1.txt"); // created behind the scenes
 
-        try (
-            BufferedReader reader = new BufferedReader(new FileReader(inputFile));
-            BufferedWriter writer = new BufferedWriter(new FileWriter(outputFile))
-        ) {
-            String line;
-            while ((line = reader.readLine()) != null) {
-                writer.write(line.toUpperCase());  // capitalize all letters
-                writer.newLine();
-            }
+        long startTime = System.currentTimeMillis();
 
-            System.out.println("Created file: " + outputFile.getAbsolutePath());
-        } catch (IOException e) {
+        Thread thread1 = new Thread(new CapitalizeFileTask("Bible.txt", "capitalized_output1.txt"));
+        Thread thread2 = new Thread(new CapitalizeFileTask("WP.txt", "capitalized_output2.txt"));
+
+
+        thread1.start();
+        thread2.start();
+
+
+        try {
+            thread1.join();
+            thread2.join();
+        } catch (InterruptedException e) {
             e.printStackTrace();
         }
 
-        File inputFile2 = new File("WP.txt");
-        File outputFile2 = new File("capitalized_output2.txt"); // created behind the scenes
 
-        try (
-            BufferedReader reader = new BufferedReader(new FileReader(inputFile2));
-            BufferedWriter writer = new BufferedWriter(new FileWriter(outputFile2))
-        ) {
-            String line;
-            while ((line = reader.readLine()) != null) {
-                writer.write(line.toUpperCase());  // capitalize all letters
-                writer.newLine();
-            }
+        long endTime = System.currentTimeMillis();
+        long duration = endTime - startTime;
 
-            System.out.println("Created file: " + outputFile2.getAbsolutePath());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
+        System.out.println("Both files have been processed.");
+        System.out.println("Time taken: " + duration + " milliseconds");
     }
 }
